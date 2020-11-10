16af3aa28e63902c448f6abdf5ffb9f065bde882
RyoNgao <thaigiui2016@gmail.com>
2020-11-10 20:05:45 +0700

    Add user 1

--- javascript/anime-list.js
+++ javascript/anime-list.js
@@ -1,22 +1,47 @@
-const themeMap = {
-    dark: "light",
-    light: "solar",
-    solar: "dark"
-};
+//getting all required elements
+const searchWrapper = document.querySelector(".search-input");
+const inputBox = searchWrapper.querySelector("input");
+const suggBox = searchWrapper.querySelector(".autocom-box");
 
-const theme = localStorage.getItem('theme') ||
-    (tmp = Object.keys(themeMap)[0],
-        localStorage.setItem('theme', tmp),
-        tmp);
-const bodyClass = document.body.classList;
-bodyClass.add(theme);
-
-function toggleTheme() {
-    const current = localStorage.getItem('theme');
-    const next = themeMap[current];
-
-    bodyClass.replace(current, next);
-    localStorage.setItem('theme', next);
+//if user press any key and release
+inputBox.onkeyup = (e)=>{
+    let userData = e.target.value; //user enetered data
+    let emptyArray = [];
+    if(userData){
+        emptyArray = suggestions.filter((data)=>{
+            //filtering array value and user char to lowerCase and return only those 
+            //word/sent which are starts with user entered word
+            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+        });
+        emptyArray = emptyArray.map((data)=>{
+            return data = '<li>'+data+'</li>';
+        });
+        console.log(emptyArray);
+        searchWrapper.classList.add("active"); //show autocomplete box
+        showSuggestions(emptyArray);
+        let allList = suggBox.querySelectorAll("li");
+        for(let i = 0; i < allList.length; i++){
+            //adding onclick attribute in all li tag
+            allList[i].setAttribute("onclick","select(this)");
+        }
+    }else{
+        searchWrapper.classList.remove("active");//hide autocomplete box
+    }
 }
 
-document.getElementById('themeButton').onclick = toggleTheme;
+function select(element){
+    let selectUserData = element.textContent;
+    inputBox.value = selectUserData; //passing the user selected list item data in textfield
+    searchWrapper.classList.remove("active"); //hide autocomplete box
+}
+
+function showSuggestions(list){
+    let listData;
+    if(!list.length){
+        userValue = inputBox.value;
+        listData = '<li>'+userValue+'</li>';
+    }else{
+        listData = list.join('');
+    }
+    suggBox.innerHTML = listData;
+}
